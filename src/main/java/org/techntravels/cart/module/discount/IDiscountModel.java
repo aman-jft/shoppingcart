fc9f7417a63b2396a0ed6924feb440880a75727a
Aman Goel <amankumar.goyal.ak@gmail.com>
2019-12-26 10:57:47 +0530

    Images

--- src/main/java/org/techntravels/cart/module/discount/IDiscountModel.java
+++
@@ -1,8 +0,0 @@
-package org.techntravels.cart.module.discount;
-
-import org.techntravels.cart.domain.Cart;
-
-public interface IDiscountModel {
-	public void setNextModel(IDiscountModel nextModel);
-	public void apply(Cart cart);
-}
